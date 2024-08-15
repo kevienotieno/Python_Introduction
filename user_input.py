f448c484ef685b7ce9142609041f54a5af9dcc44
kevienotieno <kevienotieno@gmail.com>
2024-08-15 08:46:32 +0300

    input in python

---
+++ user_input.py
@@ -0,0 +1,5 @@
+name = input("Enter your name please: ")
+age = input("What is your age? ")
+location = input("What is your current location? ")
+
+print(f"Hello {name} , you are {age} years old and live in {location}.")
